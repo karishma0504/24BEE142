2fec1858029342ccadb60ac5a0aba69929f615fd
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:15:48 +0530

    Add files via upload

---
+++ Assignment-1/10.py
@@ -0,0 +1,6 @@
+# Prompt: Convert dollars into pounds where 1 $ = 48 Rs. and 1 pound = 70 Rs.
+
+dollars = 10
+dollars_in_rs = dollars * 48  # Convert dollars to Rs.
+pounds = dollars_in_rs / 70  # Convert Rs. to pounds
+print(f"{dollars} dollars is equal to {pounds} pounds")
