9fcabe759abba42c1a885a0daa699d19aa2b4acf
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:25:54 +0530

    Add files via upload

---
+++ Assignment-2/1 (1).py
@@ -0,0 +1,11 @@
+# Prompt: Print largest and smallest values out of two
+
+a = 15
+b = 25
+
+# Finding largest and smallest
+largest = a if a > b else b
+smallest = a if a < b else b
+
+print(f"The largest value is {largest}")
+print(f"The smallest value is {smallest}")
