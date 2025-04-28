177bbaf9ba7144217db2ad8ef418df05e145301a
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:18:42 +0530

    Add files via upload

---
+++ Assignment-1/18.py
@@ -0,0 +1,10 @@
+# Prompt: Calculate area & perimeter of a rectangle. A = L*B, P = 2(L+B)
+
+L = 8  # Length of the rectangle
+B = 5  # Breadth of the rectangle
+
+area = L * B
+perimeter = 2 * (L + B)
+
+print(f"The area of the rectangle is {area}")
+print(f"The perimeter of the rectangle is {perimeter}")
