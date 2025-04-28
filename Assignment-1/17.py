eb27c332bd00814d3ab035858e901606b568fea7
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:18:17 +0530

    Add files via upload

---
+++ Assignment-1/17.py
@@ -0,0 +1,9 @@
+# Prompt: Calculate area & perimeter of a square. A = L^2, P = 4L
+
+L = 6  # Side length of the square
+
+area = L ** 2
+perimeter = 4 * L
+
+print(f"The area of the square is {area}")
+print(f"The perimeter of the square is {perimeter}")
