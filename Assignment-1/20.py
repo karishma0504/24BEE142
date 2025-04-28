6c6f377834d08ae4fae3e4b0a53ed419b88adfab
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:19:29 +0530

    Add files via upload

---
+++ Assignment-1/20.py
@@ -0,0 +1,7 @@
+# Prompt: Calculate area of a triangle. A = H*L/2
+
+H = 10  # Height of the triangle
+L = 6   # Length of the base
+
+area = (H * L) / 2
+print(f"The area of the triangle is {area}")
