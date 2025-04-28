08a2ab06e584b2c9e2f4ed7c28be5240a7e398f8
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:17:53 +0530

    Add files via upload

---
+++ Assignment-1/16.py
@@ -0,0 +1,8 @@
+# Prompt: Calculate interest where I = PRN/100
+
+P = 1000  # Principal amount
+R = 5     # Rate of interest
+N = 2     # Time period in years
+
+I = (P * R * N) / 100
+print(f"The interest is {I}")
