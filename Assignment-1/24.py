7830deed4e346034fdae409954dca69acd75b2bd
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:21:09 +0530

    Add files via upload

---
+++ Assignment-1/24.py
@@ -0,0 +1,11 @@
+# Prompt: Swap two values using a temporary variable
+
+a = 5
+b = 10
+
+# Swapping values using a temporary variable
+temp = a
+a = b
+b = temp
+
+print(f"After swapping: a = {a}, b = {b}")
