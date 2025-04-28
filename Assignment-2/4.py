1bc3180667ac43403c04d1c1e50383ed28a30f85
karishma0504 <24bee142@spt.pdpu.ac.in>
2025-04-28 15:27:57 +0530

    Add files via upload

---
+++ Assignment-2/4.py
@@ -0,0 +1,8 @@
+# Prompt: Check whether a given number is divisible by 10 or not
+
+num = 50
+
+if num % 10 == 0:
+    print(f"{num} is divisible by 10")
+else:
+    print(f"{num} is not divisible by 10")
